44bfccaaa76875bb5c121735d3665d8529ee69b5
Harsh Ramesh <harshavardhan_ramesh@outlook.com>
2026-01-26 18:29:32 -0600

    [Sprint 1][Task 4] test: add helper functions for all routes - added internal helper functions `get()` & `post()` to alleviate boilerplate. - added public functions for all routes from `api_schema.yml`.

--- auth-service/tests/api/helpers.rs
+++ auth-service/tests/api/helpers.rs
@@ -23,9 +23,49 @@ impl TestApp {
         }
     }
 
+    #[inline]
     pub async fn get_root(&self) -> reqwest::Response {
+        self.get("/").await
+    }
+
+    #[inline]
+    pub async fn get_signup(&self) -> reqwest::Response {
+        self.post("/signup").await
+    }
+
+    #[inline]
+    pub async fn get_login(&self) -> reqwest::Response {
+        self.post("/login").await
+    }
+
+    #[inline]
+    pub async fn get_logout(&self) -> reqwest::Response {
+        self.post("/logout").await
+    }
+
+    #[inline]
+    pub async fn get_verify_2fa(&self) -> reqwest::Response {
+        self.post("/verify-2fa").await
+    }
+
+    #[inline]
+    pub async fn get_verify_token(&self) -> reqwest::Response {
+        self.post("/verify-token").await
+    }
+
+    #[inline]
+    async fn get(&self, addr: &str) -> reqwest::Response {
         self.http_client
-            .get(&format!("{}/", &self.address))
+            .get(&format!("{}{addr}", &self.address))
+            .send()
+            .await
+            .expect("could not execute request")
+    }
+
+    #[inline]
+    async fn post(&self, addr: &str) -> reqwest::Response {
+        self.http_client
+            .post(&format!("{}{addr}", &self.address))
             .send()
             .await
             .expect("could not execute request")
